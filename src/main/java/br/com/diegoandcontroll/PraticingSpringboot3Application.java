1ca0eeac1a714ee6f309c0eb15c30a8e9493fd2e
diegoandcontroll <lukasxdp2@gmail.com>
2023-04-29 23:36:17 -0300

    dockerize with jib service app and postgresql

--- src/main/java/br/com/diegoandcontroll/PraticingSpringboot3Application.java
+++ src/main/java/br/com/diegoandcontroll/PraticingSpringboot3Application.java
@@ -3,7 +3,11 @@ package br.com.diegoandcontroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Jwt auth example", version = "1.0", description = "Simple example jwt auth"))
 public class PraticingSpringboot3Application {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/diegoandcontroll/config/SecurityConfiguration.java
+++ src/main/java/br/com/diegoandcontroll/config/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package br.com.diegoandcontroll.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,19 +22,32 @@ public class SecurityConfiguration {
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
   private final LogoutHandler logoutHandler;
-
+  private static final String[] AUTH_WHITELIST = {
+    // -- Swagger UI v2
+    "/v2/api-docs",
+    "/swagger-resources",
+    "/swagger-resources/**",
+    "/configuration/ui",
+    "/configuration/security",
+    "/swagger-ui.html",
+    "/webjars/**",
+    // -- Swagger UI v3 (OpenAPI)
+    "/v3/api-docs/**",
+    "swagger-ui/",
+    "/swagger-ui/index.html",
+    "/swagger-ui/**",
+    "/api/v1/auth/**"
+    // other public endpoints of your API may be appended to this array
+};
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf()
         .disable()
         .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**")
-        .permitAll()
+        .requestMatchers(AUTH_WHITELIST).permitAll()
         .and()
         .authorizeHttpRequests()
-        .requestMatchers("api/v1/categories/**")
-        .permitAll()
         .anyRequest()
         .authenticated()
         .and()
@@ -45,7 +60,11 @@ public class SecurityConfiguration {
         .logoutUrl("/api/v1/auth/logout")
         .addLogoutHandler(logoutHandler)
         .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
-
     return http.build();
   }
+
+  // @Bean
+  // public WebSecurityCustomizer webSecurityCustomizer() {
+  //   return (web) -> web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+  // }
 }
